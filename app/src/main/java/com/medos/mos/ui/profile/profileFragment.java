7fcd86f1c711f480678c0b084dc3e623be72ea74
hazeke94 <1700446@sit.singaporetech.edu.sg>
2019-10-28 11:27:25 +0800

    #12 Skeleton for Update Profile - Added skeleton code for Profile Page - Updated xml for Profile page

--- app/src/main/java/com/medos/mos/ui/profile/profileFragment.java
+++ app/src/main/java/com/medos/mos/ui/profile/profileFragment.java
@@ -8,25 +8,49 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.medos.mos.R;
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class profileFragment extends Fragment {
-
-
-    public profileFragment() {
-        // Required empty public constructor
-    }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
+        final Button btnProfile = root.findViewById(R.id.btnProfile);
+        Button btnUpdateProfile = root.findViewById(R.id.btnUpdateProfile);
+        Button btnCancelProfile = root.findViewById(R.id.btnCancelProfile);
+
+        final LinearLayout editLayout = root.findViewById(R.id.EditLayout);
+
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //btnProfile
+                btnProfile.setVisibility(View.GONE);
+                //show edit layout
+                editLayout.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btnUpdateProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnCancelProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //btnProfile
+                btnProfile.setVisibility(View.VISIBLE);
+                //show edit layout
+                editLayout.setVisibility(View.GONE);
+            }
+        });
         return root;
     }
 
